faeea405a1ee0e463340a58e756d256e237fee00
JavierHevia <javierhc22@gmail.com>
2020-06-04 17:35:41 -0600

    :D

---
+++ Ubuntu/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Eduardo Javier Hevia Calderòn \n 201313898 \n\n');
+});
+
+app.listen(PORT, HOST);
+console.log('Arrancado en http://'+HOST+':'+PORT+ '\nEduardo Javier Hevia Calderon\n201313898\n');
